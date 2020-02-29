386c7c56f2dc269bd2573b23d97597ebed655d38
Ruan Montelo <ruanmontelo@gmail.com>
2020-02-29 12:26:06 -0300

    Creating environment variable for the API Key.

--- PokemonGOAPI/Configurations/Config.cs
+++ PokemonGOAPI/Configurations/Config.cs
@@ -1,14 +1,14 @@
-﻿namespace PokemonGOAPI.Configurations
+﻿using System;
+
+namespace PokemonGOAPI.Configurations
 {
     public static class Config
     {
-        private static string _xRapidapiKey => "4d05572d05msh8850743a12d5d73p17280fjsn86033a0fdb9a";
-
         public static string APIKey
         {
             get
             {
-                return _xRapidapiKey;
+                return Environment.GetEnvironmentVariable("XRAPIDAPIKEY");
             }
         }
     }
